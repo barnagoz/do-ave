72c5274515dd44f30a2000920421865db7c62143
Barnabás Gőz <barnagoz@icloud.com>
2023-05-23 23:40:06 +0200

    feat: add google analytics and favicons

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,47 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="hu">
+      <Head>
+        <title>AVE Napok 2023</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LNMCZZKZK6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LNMCZZKZK6');
+        `}
+        </Script>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
