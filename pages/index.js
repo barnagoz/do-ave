71e60e4de6d5461ad725d92cf5b42f59d9f8755d
Barnabás Gőz <barnagoz@icloud.com>
2023-05-23 19:01:12 +0200

    feat: update programs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import { Inter } from "next/font/google";
 import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
   Box,
+  Button,
   Card,
   CardBody,
+  CardFooter,
   CardHeader,
   Flex,
   Heading,
@@ -11,6 +16,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -70,27 +76,27 @@ export default function Home() {
           <Text ml={2} mb={2} fontSize={"16"} textColor={"gray.600"}>
             Kattints a programnapokra, hogy megtudd, melyik iskolában mi lesz!
           </Text>
-          <Card w={"100%"} onClick={() => setSelectedDay(0)}>
+          <Card w={"100%"} onClick={() => setSelectedDay(0)} cursor={"pointer"}>
             <CardHeader>
               <Heading fontSize={"20"}>Szerda (05.24.) - VPG</Heading>
             </CardHeader>
-            <CardBody>
+            <CardBody pt={0}>
               <Text>Buzidítószöveg, hogy miért ide kattints először</Text>
             </CardBody>
           </Card>
-          <Card w={"100%"} onClick={() => setSelectedDay(1)}>
+          <Card w={"100%"} onClick={() => setSelectedDay(1)} cursor={"pointer"}>
             <CardHeader>
               <Heading fontSize={"20"}>Csütörtök (05.25.) - Eötvös</Heading>
             </CardHeader>
-            <CardBody>
+            <CardBody pt={0}>
               <Text>Buzidítószöveg, hogy miért ide kattints először</Text>
             </CardBody>
           </Card>
-          <Card w={"100%"} onClick={() => setSelectedDay(2)}>
+          <Card w={"100%"} onClick={() => setSelectedDay(2)} cursor={"pointer"}>
             <CardHeader>
               <Heading fontSize={"20"}>Péntek (05.26.) - Apáczai</Heading>
             </CardHeader>
-            <CardBody>
+            <CardBody pt={0}>
               <Text>
                 Buzidítószöveg, hogy miért ide kattints először: MERT EJG!
               </Text>
@@ -99,43 +105,249 @@ export default function Home() {
         </Flex>
         <Flex
           w={"70%"}
-          h={"100%"}
+          h={"calc(80vh - 4px)"}
           minH={"calc(80vh - 4px)"}
+          maxH={"calc(80vh - 4px)"}
+          overflow={"scroll"}
           rounded={"lg"}
           justify={"flex-start"}
           direction={"column"}
           background={"#F7F6C5"}
-          gap={2}
+          gap={4}
           p={2}
         >
           {selectedDay === 0 && (
             <>
-              <Heading mb={-2} mt={3} ml={2} fontSize={"24"}>
-                Szerda (05.24.) - Apáczai
+              <Heading mt={3} ml={2} fontSize={"24"}>
+                Szerda (05.24.) - VPG
               </Heading>
-              <Text ml={2} mb={2} fontSize={"16"} textColor={"gray.600"}>
-                Ide jön a szöveg
-              </Text>
+              <Card rounded="lg" size="sm" w={"100%"}>
+                <CardHeader>
+                  <Heading fontSize={"22"}>Kvízshow</Heading>
+                </CardHeader>
+                <CardBody pt={0}>
+                  <Text>
+                    <b>Időpont</b>: 15:45-17:00
+                  </Text>
+                  <Text>
+                    <b>Helyszín</b>: aula
+                  </Text>
+                </CardBody>
+                <CardFooter>
+                  <Box w={"100%"}>
+                    <Alert
+                      w="100%"
+                      status="warning"
+                      variant="subtle"
+                      rounded={"md"}
+                      mb={1}
+                    >
+                      <AlertIcon />
+                      <AlertTitle mr={2}>
+                        A kvízshowra jelentkezés szükséges!
+                      </AlertTitle>
+                    </Alert>
+                    <Link
+                      href={
+                        "https://forms.office.com/pages/responsepage.aspx?id=32oi4RmZYEabUvmJndFcECupqCAFjJhLg05L6wP2haNUOFMwM0ZGTlpIUkVSWUdPVkRRWkhNWDZTQy4u&fbclid=IwAR3EaGW6NID2GgyUwJ9DDvLwA8V0OqJTTpgtw0a_7u07X9VrzPbkcanfwTY"
+                      }
+                    >
+                      <Button w="100%" colorScheme={"blue"}>
+                        Jelentkezés
+                      </Button>
+                    </Link>
+                  </Box>
+                </CardFooter>
+              </Card>
+              <Card rounded="lg" size="sm" w={"100%"}>
+                <CardHeader>
+                  <Heading fontSize={"22"}>Jóslás</Heading>
+                </CardHeader>
+                <CardBody pt={0}>
+                  <Text fontStyle="italic" p={4} pt={0}>
+                    Jósoltass magadnak szerencsét a VPG aulájának egy félreeső
+                    részében! Tarot kártya olvasás, számmisztika és tenyérjóslás
+                    – tudd meg a jövőd!
+                  </Text>
+                  <Text>
+                    <b>Időpont</b>: 15:45-17:30
+                  </Text>
+                  <Text>
+                    <b>Helyszín</b>: aula
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card rounded="lg" size="sm" w={"100%"}>
+                <CardHeader>
+                  <Heading fontSize={"22"}>Röplabda</Heading>
+                </CardHeader>
+                <CardBody pt={0}>
+                  <Text fontStyle="italic" p={4} pt={0}>
+                    A VPG külső udvarán játszhatsz a barátaiddal vagy akár más
+                    iskola tanulóival is barátságos meccseket, melyek időtartama
+                    10-10 perc. A meccseket fújni, a pontokat számolni fogják a
+                    VPG tanulói, a szórakozás garantált!
+                  </Text>
+                  <Text>
+                    <b>Időpont</b>: 15:45-17:15
+                  </Text>
+                  <Text>
+                    <b>Helyszín</b>: udvar
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card rounded="lg" size="sm" w={"100%"}>
+                <CardHeader>
+                  <Heading fontSize={"22"}>Szabadulószoba</Heading>
+                </CardHeader>
+                <CardBody pt={0}>
+                  <Text fontStyle="italic" p={4} pt={0}>
+                    A VPG rajztermében csapatokban próbálhattok majd szerencsét.
+                    Ha szeretsz csapatban dolgozni és nem ijedsz meg a
+                    kihívásoktól, akkor ott a helyed. Ha nem fix csapattal
+                    érkezel, az is teljesen rendben van.
+                  </Text>
+                  <Text>
+                    <b>Időpont</b>: 15:45-17:00
+                  </Text>
+                  <Text>
+                    <b>Helyszín</b>: rajzterem
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card rounded="lg" size="sm" w={"100%"}>
+                <CardHeader>
+                  <Heading fontSize={"22"}>Ismerkedős játékok</Heading>
+                </CardHeader>
+                <CardBody pt={0}>
+                  <Text fontStyle="italic" p={4} pt={0}>
+                    A VPG-s program zárásaként lehetőséget adunk nektek, hogy a
+                    többi programon szimpatikussá vált tanulókat közelebbről is
+                    megismerhessétek különféle ismerkedős játékok keretében.
+                  </Text>
+                  <Text>
+                    <b>Időpont</b>: 17:05-17:30
+                  </Text>
+                  <Text>
+                    <b>Helyszín</b>: aula
+                  </Text>
+                </CardBody>
+              </Card>
             </>
           )}
           {selectedDay === 1 && (
             <>
-              <Heading mb={-2} mt={3} ml={2} fontSize={"24"}>
-                Csütörtök (05.25.) - VPG
+              <Heading mt={3} ml={2} fontSize={"24"}>
+                Csütörtök (05.25.) - Eötvös
               </Heading>
-              <Text ml={2} mb={2} fontSize={"16"} textColor={"gray.600"}>
-                Ide jön a szöveg
-              </Text>
+              <Card rounded="lg" size="sm" w={"100%"}>
+                <CardHeader>
+                  <Heading fontSize={"22"}>Streetball</Heading>
+                </CardHeader>
+                <CardBody pt={0}>
+                  <Text fontStyle="italic" p={4} pt={0}>
+                    Az Eötvösben az iskolák csapatai össze mérhetik kosárlabda
+                    tudásuk. A csapatok 3 főből állhatnak és lehetőség van 1
+                    csere regisztrálására!
+                  </Text>
+                  <Text>
+                    <b>Időpont</b>: 16:15-18:30
+                  </Text>
+                  <Text>
+                    <b>Helyszín</b>: 3. emeleti tornacsarnok
+                  </Text>
+                </CardBody>
+                <CardFooter>
+                  <Box w={"100%"}>
+                    <Alert
+                      w="100%"
+                      status="warning"
+                      variant="subtle"
+                      rounded={"md"}
+                      mb={1}
+                    >
+                      <AlertIcon />
+                      <AlertTitle mr={2}>
+                        A streetballra jelentkezés szükséges!
+                      </AlertTitle>
+                    </Alert>
+                    <Link href={"https://forms.gle/a4rJ9bnrcNTKsj8GA"}>
+                      <Button w="100%" colorScheme={"blue"}>
+                        Jelentkezés
+                      </Button>
+                    </Link>
+                  </Box>
+                </CardFooter>
+              </Card>
+              <Card rounded="lg" size="sm" w={"100%"}>
+                <CardHeader>
+                  <Heading fontSize={"22"}>Társasjátékozás</Heading>
+                </CardHeader>
+                <CardBody pt={0}>
+                  <Text fontStyle="italic" p={4} pt={0}>
+                    A sportesemény mellett a résztvevőknek van lehetőségük
+                    különféle társasjátékokban összemérni tudásuk. Gyertek el és
+                    játszatok barátaitokkal!
+                  </Text>
+                  <Text>
+                    <b>Időpont</b>: 16:15-18:30
+                  </Text>
+                  <Text>
+                    <b>Helyszín</b>: 307-es terem
+                  </Text>
+                  <Text
+                    fontStyle="italic"
+                    textColor={"gray.600"}
+                    fontSize={"12"}
+                  >
+                    Az Eötvös Társastár jóvoltából.
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card rounded="lg" size="sm" w={"100%"}>
+                <CardHeader>
+                  <Heading fontSize={"22"}>Kocsma</Heading>
+                </CardHeader>
+                <CardBody pt={0}>
+                  <Text fontStyle="italic" p={4} pt={0}>
+                    Ha elfáradtál a szurkolásban, vagy csak egy kis
+                    felfrissülésre vágysz gyere el a “kocsmába”! Üdítővel és
+                    gofrival várunk mindenkit! Ne izgulj! A meccsekről sem
+                    maradsz le, mivel élő közvetítésen nézheted az esemenyeket!
+                  </Text>
+                  <Text>
+                    <b>Időpont</b>: 16:15-18:30
+                  </Text>
+                  <Text>
+                    <b>Helyszín</b>: 308-as terem
+                  </Text>
+                </CardBody>
+              </Card>
             </>
           )}
           {selectedDay === 2 && (
             <>
-              <Heading mb={-2} mt={3} ml={2} fontSize={"24"}>
-                Péntek (05.26.) - Eötvös
+              <Heading mt={3} ml={2} fontSize={"24"}>
+                Péntek (05.26.) - Apáczai
               </Heading>
-              <Text ml={2} mb={2} fontSize={"16"} textColor={"gray.600"}>
-                Ide jön a szöveg
-              </Text>
+              <Card rounded="lg" size="sm" w={"100%"}>
+                <CardHeader>
+                  <Heading fontSize={"22"}>Casino</Heading>
+                </CardHeader>
+                <CardBody pt={0}>
+                  <Text fontStyle="italic" p={4} pt={0}>
+                    Gyere el és játssz a szerencsével! A kaszinó ajtai nyitva
+                    állnak ahol pókerrel, black jackkel, unoval illetve
+                    különféle társasokkal várunk Titeket!
+                  </Text>
+                  <Text>
+                    <b>Időpont</b>: 17:00-19:00
+                  </Text>
+                  <Text>
+                    <b>Helyszín</b>: színházterem
+                  </Text>
+                </CardBody>
+              </Card>
             </>
           )}
         </Flex>
